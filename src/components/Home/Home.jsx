bebff90134b25baac535516a51c5aa09864b4c4f
azhou05 <azhou05@terpmail.umd.edu>
2026-08-16 12:31:22 -0400

    added welcome to homepage

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import Welcome from './Welcome';
+
+export default function Home() {
+    return(
+    <div>   
+        <Welcome />
+    </div>
+    );
+}
---
+++ src/components/Home/Welcome.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+export default function Welcome(props) {
+    return( <div>
+        <h1>
+            Welcome to UMCP TASA!
+        </h1>
+        <p>  
+            The <b>Taiwanese American Student Association (TASA)</b> at the
+                    University of Maryland, College Park is a social and cultural
+                    student organization dedicated to celebrating Taiwanese culture
+                    on campus. TASA at UMCP is dedicated to developing and
+                    maintaining Taiwanese American student life and organizational
+                    relations on campus, as well as developing relations with
+                    Taiwanese and Taiwanese American communities nationwide. Our
+                    community is composed of and open to people from all backgrounds
+                    interested in learning about and celebrating Taiwanese culture.
+                    We hold general meetings on <b>Mondays from 7pm - 9pm</b> and
+                    host many events throughout the school year. Check out more
+                    here!
+        </p>
+    </div>)
+}
